f3ed19c59005e7e43e1207c4e81e7e5563085dfc
Jedidah <jedidahwambuguw@gmail.com>
2023-01-08 16:45:17 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for content in dir(hidden_4):
+    
+    names = dir(hidden_4)
+    for content in names:
         if content [:2] != "__":
             print(content)
 
