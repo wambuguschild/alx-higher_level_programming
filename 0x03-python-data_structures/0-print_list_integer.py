2e2feca8af7de3d030f66fd41933deed87499959
Jedidah <jedidahwambuguw@gmail.com>
2023-01-09 22:15:31 +0300

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_list_interger(my_list=[]):
+"""Print items in a list"""
+
+
+def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
