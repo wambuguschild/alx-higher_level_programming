b86ec5ba60def70e4ab9c5213e9ce9e6a3ce1a65
Jedidah <jedidahwambuguw@gmail.com>
2023-01-09 22:43:56 +0300

    made file executable

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the sum of a and b."""
     from add_0 import add
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
