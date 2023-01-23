1e40be317e9b066bc9396b0dfbdd278a1a3cfe0d
Jedidah <jedidahwambuguw@gmail.com>
2023-01-23 22:28:10 +0300

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    
+    i = 0
+    
+    for j in range(x):
+        
+        try:
+            print(my_list[j], end=" ")
+            
+            n+= 1
+
+        except IndexError:
+        
+            break
+        
+        print()
+
+        return n
+
