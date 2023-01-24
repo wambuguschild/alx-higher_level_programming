ebb324235040cf66d8680cfbccdd9978e04e05fc
Jedidah <jedidahwambuguw@gmail.com>
2023-01-24 22:26:27 +0300

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 def raise_exception_msg(message=""):
-    """Raises a NameError with a message"""
+    """Raise a NameError exception with a message."""
     raise NameError(message)
 
