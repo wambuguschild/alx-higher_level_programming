626d34d48dfcaa68bf734e23c7c9a23dace88968
Jedidah <jedidahwambuguw@gmail.com>
2023-03-15 22:26:47 +0300

    Added First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
 -- A script that creates a table called first_table in the current database in your MySQL server.
 
 CREATE first_table (
-	id INT, name VARCHAR(256));
-
+	id INT,
+	name VARCHAR(256)
+);
