ea73d7af74b3aa620255e52ccfd0fa4ff6dccffc
Jedidah <jedidahwambuguw@gmail.com>
2023-03-16 23:04:54 +0300

    fixed errors on Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,10 @@
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-	FROM tv_shows
-	JOIN tv_show_ratings
-	ON tv_shows.id = tv_show_ratings.show_id
-	GROUP BY tv_shows.title
+SELECT tv_shows.genres, SUM(tv_show_ratings.rate) AS rating
+	FROM tv_genres
+	INNER JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_show_ratings
+	ON tv_show_genres.show_id = tv_show_ratings.show_id
+	GROUP BY tv_genres.id
 	ORDER BY rating DESC;
