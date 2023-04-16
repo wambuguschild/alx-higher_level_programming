f403d6f45d30075dd7162b177b37551e3215b087
Jedidah <jedidahwambuguw@gmail.com>
2023-04-16 21:37:18 +0300

    Added SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,16 +5,16 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    results = cur.fetchall()
-    for row in results:
-        if row[1][0] == 'N':
-            print(row)
+    cur.execute("SELECT*FROM states WHERE name LIKE '{:s}' ORDER BY states.id ASC;" .format(sys.argv[4]))
+    for state in cur.fetchall():
+        print(state)
     cur.close()
     db.close()
