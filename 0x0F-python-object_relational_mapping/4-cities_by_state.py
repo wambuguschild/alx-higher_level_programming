352de7ae47681c1e0daaa359c89be35696cf8ad7
Jedidah <jedidahwambuguw@gmail.com>
2023-04-21 17:14:15 +0300

    Added Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ A script that lists all cities from the database hbtn_0e_4_usa"""
 
+import MySQLdb
+import sys
+
 if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost",
@@ -11,7 +14,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
             FROM cities\
-            INNER JOIN states ON cities.states_id = states.id\
+            INNER JOIN states ON cities.state_id = states.id\
             ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
