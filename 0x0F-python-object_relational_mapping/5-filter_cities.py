e1ea4605ea6a9ed9dcfcbced90960a478f23b6fb
Jedidah <jedidahwambuguw@gmail.com>
2023-04-21 21:22:41 +0300

    Added All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,22 +5,21 @@ and lists all cities of that state, using the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM cities\
-            INNER JOIN states ON cities.state_id = states.id\
-            WHERE states.name = %s\
+    cur.execute("SELECT cities.name\
+            FROM states\
+            INNER JOIN cities ON states.id = cities.state_id\
+            WHERE states.name LIKE %s\
             ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
+    print(", ".join(row[0] for row in rows))
     cur.close()
     db.close()
