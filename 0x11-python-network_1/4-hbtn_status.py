f61f9d659c5be2c7c48f905cb1206afd55a2bab2
Jedidah <jedidahwambuguw@gmail.com>
2023-05-01 14:17:35 -0700

    Added What is my status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     response.raise_for_status()
-    content = response.test
+    content = response.text
 
     print('Body response:')
     print(f'\t- type: {type(content)}')
